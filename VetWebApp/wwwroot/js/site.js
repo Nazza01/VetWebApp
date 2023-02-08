1435df100e6276b51821c908ea025ea3b4cdc70d
Nat Ervin <nate_thomas_95@hotmail.com>
2023-02-08 16:41:54 +1030

    Commiting SRA Laptop changes

--- VetWebApp/wwwroot/js/site.js
+++ VetWebApp/wwwroot/js/site.js
@@ -3,3 +3,45 @@
 
 // Write your JavaScript code.
 
+// Increases font size for each element with the class name "font-access"
+function incSize(e) {
+	var elements = document.getElementsByClassName("font-access");
+	for (var i = 0; i < elements.length; i++)
+	{
+		console.log("Increasing font size for: " + elements[i].style.fontFamily);
+		elements[i].style.fontSize = 'xx-large';
+	}
+}
+
+function resetSize(e) {
+	var elements = document.getElementsByClassName("font-access");
+
+	for (var i = 0; i < elements.length; i++)
+	{
+		console.log("Resetting font size for: " + elements[i].style.fontFamily);
+		elements[i].style.fontSize = 'large';
+	}
+}
+
+function decSize(e) {
+	var elements = document.getElementsByClassName("font-access");
+
+	for (var i = 0; i < elements.length; i++)
+	{
+		console.log("Changing font size for: " + elements[i].style.fontFamily);
+		elements[i].style.fontSize = 'small';
+	}
+}
+
+// Font Styling
+
+function fontType(event) {
+	var fontfam = event.value;
+	var elements = document.getElementsByClassName("font-access");
+
+	for (var i = 0; i < elements.length; i++) {
+		console.log("Changing font type for: " + elements[i].style.fontFamily);
+		elements[i].style.fontFamily = fontfam;
+	}
+}
+

8f84b7ff6cd858e57f6f6d5d1b28c6074a9b40e2
Nat Ervin <nate_thomas_95@hotmail.com>
2023-03-07 15:14:40 +1030

    Fixed animal selection persisting between browser restarts, need to fix the locate page again though

--- VetWebApp/Content/js/site.js
+++ VetWebApp/Content/js/site.js
@@ -3,24 +3,35 @@
 
 // Write your JavaScript code.
 
+function load() {
+	setImage(localStorage.getItem('img'));
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundSize = "cover";
+}
 
 function setImage(index)
 {
 	var bgimg = document.body.style;
+
 	if (index == "dog") {
 		bgimg.backgroundImage = "url('/Content/backgrounds/dog.jpg')";
+		localStorage.setItem("img", "dog");
 	}
 	else if (index == "cat") {
 		bgimg.backgroundImage = "url('/Content/backgrounds/cat.jpg')";
+		localStorage.setItem("img", "cat");
 	}
 	else if (index == "both") {
 		bgimg.backgroundImage = "url('/Content/backgrounds/both.jpg')";
+		localStorage.setItem("img", "both");
 	}
-	console.log("Changing background to: " + index);
+	console.log("Changing background to: " + localStorage.getItem("img"));
 }
 
 function resetImage() {
 	console.log("Resetting background color");
 	document.body.style.backgroundImage = "url('')";
+	localStorage.setItem("img", "none");
 }
 
+window.onload = load();

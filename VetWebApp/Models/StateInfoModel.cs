3fdcd37e1cfe77e3c481ebd998ea9c60c510a945
Nazza01 <nate_thomas_95@hotmail.com>
2023-03-13 22:45:46 +1030

    Adding admin page model and the state of the server

---
+++ VetWebApp/Models/StateInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace VetWebApp.Models
+{
+	public class StateInfoModel
+	{
+		public StateInfoModel()
+		{
+			ServerState = new List<string> { "" };
+		}
+		public List<string> ServerState { get; set; }
+	}
+}
+
